2f468034c954df23c88f036876cd4ebbf0826467
francescorossetti03 <francesco.rossettiiii@gmail.com>
2024-01-02 15:01:41 +0100

    aggiunta classe CustomRender per colorare di rosso le corse cancellate nel tabellone del corse

---
+++ Applicativo/Applicativo/src/main/java/unnamed/CustomRenderer.java
@@ -0,0 +1,28 @@
+package unnamed;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class CustomRenderer extends DefaultTableCellRenderer {
+    private ArrayList<Boolean> booleanList;
+
+    public CustomRenderer(ArrayList<Boolean> booleanList) {
+        this.booleanList = booleanList;
+    }
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        Component comp = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+
+        if (row < booleanList.size() && booleanList.get(row)) {
+            comp.setBackground(new Color(255, 120, 120));
+        } else {
+            comp.setBackground(table.getBackground());
+            comp.setForeground(table.getForeground());
+        }
+
+        return comp;
+    }
+}
