c30d7f3a50a15ef9fdaae5ddce801061fbee0667
riccardoregina <riccardoregina04@gmail.com>
2024-01-22 18:45:42 +0100

    Modifica metodo isAlpha: adesso permette gli spazi e gli apostrofi

--- Applicativo/Applicativo/src/main/java/unnamed/NonStandardStringFunctions.java
+++ Applicativo/Applicativo/src/main/java/unnamed/NonStandardStringFunctions.java
@@ -1,10 +1,10 @@
 package unnamed;
 
-import java.util.BitSet;
-
 public class NonStandardStringFunctions {
     public static boolean isAlpha (String str) {
         for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == ' ' || c == '\'') continue;
             if (!Character.isLetter(str.charAt(i))) {
                 return false;
             }
